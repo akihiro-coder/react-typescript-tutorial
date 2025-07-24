4fd13a46f72a59064cc16bf8d321ea922faca997
akihirookuda <ScuderiaFerrariSebastian@gmail.com>
2025-07-24 15:46:30 +0900

    構文修正

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,31 +1,35 @@
 import { useState } from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
-import ProjectForm  from "./ProjectForm.tsx";
+import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
   projects: Project[];
   onSave: (project: Project) => void;
 }
 
-function ProjectList ({ projects, onSave }: ProjectListProps) {
-    const [projectBeingEdited, setProjectBeingEdited] = useState({});
+function ProjectList({ projects, onSave }: ProjectListProps) {
+    // projectBeingEdited: 現在編集中のプロジェクトを保持する状態（編集していないときは null ）
+    // setProjectBeingEdited: 編集中のプロジェクトを更新する関数
+    const [projectBeingEdited, setProjectBeingEdited] = useState<Project | null>(null);
+
     const handleEdit = (project: Project) => {
         setProjectBeingEdited(project);
     };
+
     const cancelEditing = () => {
-        setProjectBeingEdited({});
+        setProjectBeingEdited(null);
     };
 
     return (
-        <div className={"row"}>
+        <div className="row">
             {projects.map((project) => (
-                <div key={project.id} className={"cols-sm"}>
+                <div key={project.id} className="cols-sm">
                     {project === projectBeingEdited ? (
                         <ProjectForm
                             onSave={onSave}
-                            onCancel={cancelEditing}/
-                        >
+                            onCancel={cancelEditing}
+                        />
                     ) : (
                         <ProjectCard
                             project={project}
@@ -33,7 +37,7 @@ function ProjectList ({ projects, onSave }: ProjectListProps) {
                         />
                     )}
                 </div>
-            ))},
+            ))}
         </div>
     );
 }
